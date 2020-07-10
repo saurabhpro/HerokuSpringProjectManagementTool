545dc2f3f3553d2c07595954ff8f22777cd7a8ae
Saurabh Kumar <saurabhk15@outlook.com>
2020-07-11 00:20:43 +0530

    react headers with link

---
+++ src/main/resources/static/precache-manifest.cf430fa68f7fe933b35eea18563dd9ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e2f1c6d8af65961a39d7ae585a4f48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f763ab1eb83610ff29ee",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4cbd8295fa4204fe37d1",
+    "url": "/static/css/main.aaf34e0f.chunk.css"
+  },
+  {
+    "revision": "f763ab1eb83610ff29ee",
+    "url": "/static/js/2.94798d32.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.94798d32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cbd8295fa4204fe37d1",
+    "url": "/static/js/main.4980ca6a.chunk.js"
+  },
+  {
+    "revision": "28d63865b28c8964a27e",
+    "url": "/static/js/runtime-main.f9d4b775.js"
+  },
+  {
+    "revision": "e659b517756181e22dfff1aea2f3b4a2",
+    "url": "/static/media/PageNotFound.e659b517.png"
+  }
+]);
