6861e48ccd48b269f1837b8dfce0e82c42de9096
Saurabh Kumar <saurabhk15@outlook.com>
2020-07-10 08:50:19 +0530

    adding static web

---
+++ src/main/resources/static/precache-manifest.5d2b5cbbd466d178c5c67ac511d10044.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4966ac2638277088d329f664976b60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f624a51258e2efc6df5d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7b31f5ddb6803e81ba36",
+    "url": "/static/css/main.aaf34e0f.chunk.css"
+  },
+  {
+    "revision": "f624a51258e2efc6df5d",
+    "url": "/static/js/2.a45209f9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a45209f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b31f5ddb6803e81ba36",
+    "url": "/static/js/main.2a500826.chunk.js"
+  },
+  {
+    "revision": "28d63865b28c8964a27e",
+    "url": "/static/js/runtime-main.f9d4b775.js"
+  },
+  {
+    "revision": "e659b517756181e22dfff1aea2f3b4a2",
+    "url": "/static/media/PageNotFound.e659b517.png"
+  }
+]);
